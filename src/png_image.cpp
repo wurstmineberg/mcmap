19fe462bfa7b706270e7f0be2d8062797fa16a7b
Stefan Graupner <stefan.graupner@gmail.com>
2013-11-08 17:12:20 +0100

    basic image structure.

--- src/png_image.cpp
+++ src/png_image.cpp
@@ -1,33 +1,88 @@
-#include "png_reader.hpp"
+#include "png_image.hpp"
 
 using namespace std;
 
 namespace fs = boost::filesystem;
 
+
 namespace mcmap
 {
-	png_reader::png_reader(fs::path *path)
+  // custom readers and writers for fstream
+  void png_fstream_read(png_structp pngPtr, png_bytep data, png_size_t length);
+  void png_fstream_write(void);
+
+  png_image::png_image(fs::path p)
 	{
-    this->path = path;
-    this->fp.open(path->string().c_str(), ios::binary);
+    this->fp.open(path->string().c_str(), ios::read | ios::binary);
 	}
 
-  png_reader::~png_reader()
+  png_image(long width, long height)
   {
-    this->fp.close();
+
   }
 
-	png_structp png_reader::get_png_data()
+  png_image::~png_image()
+  {
+    if (this->fp.is_open()) this->fp.close();
+  }
+
+	bool png_image::load()
 	{
-    if (!this->validate())
+    // validate stream and init png structures
+    if (!this->validate() && !this->init_png_structs()) return false;
+
+
+    return true;
+  }
+
+  bool png_image::validate_stream()
+  {
+    char *testbuf = (char *)malloc(sizeof(char) * 8);
+    bool valid = false;
+    
+    this->fp.read(testbuf, 8);
+    valid = (this->fp.good() && png_sig_cmp(&testbuf, 0, 8));
+    this->fp.seekg(0, is.beg);
+
+    free(testbuf);
+
+    return valid;
+  }
+
+  bool png_image::init_png_structs()
+  {
+    this->png_ptr = png_create_read_struct(PNG_LIBPNG_VERSION_STRING, NULL, NULL, NULL);
+
+    if (this->png_ptr)
     {
-      return NULL;
+      this->info_ptr = png_create_info_struct(png_ptr);
+
+      if (!this->info_ptr)
+      {
+        png_destroy_read_struct(&this->png_ptr, (png_infopp)NULL, (png_infopp)NULL);
+        return false;
+      }
+    } else return false;
+
+    if (setjmp(png_jmpbuf(this->pngPtr))) {
+      png_destroy_read_struct(&this->pngPtr, &this->infoPtr, (png_infopp)NULL);
+      
+      if (this->rowPtrs != NULL) delete [] this->rowPtrs;
+
+      cerr << "ERROR: An error occured while reading the PNG file" << endl;
+
+      return false;
     }
 
+    return true;
+  }
+
+  void png_fstream_read(png_structp pngPtr, png_bytep data, png_size_t length)
+  {
 
   }
 
-  bool png_reader::validate()
+  void png_fstream_write(void)
   {
 
   }
--- src/png_image.hpp
+++ src/png_image.hpp
@@ -1,5 +1,5 @@
-#ifndef __png_reader_hpp
-#define __png_reader_hpp
+#ifndef __png_image_hpp
+#define __png_image_hpp
 
 #include <iostream>
 #include <fstream>
@@ -11,17 +11,35 @@
 
 namespace mcmap
 {
-	class png_reader
+  /**
+   * Wrapper class for PNG handling.
+   *
+   * Image data is always expected in RGBA when writing 
+   * or transformed to RGBA when reading.
+   **/
+	class png_image
 	{
 	public:
-		png_reader(boost::filesystem::path *path);
-		png_structp get_png_data();
+		png_image(boost::filesystem::path p);
+    png_image(long width, long height);
+    ~png_image();
 
 	private:
-		boost::filesystem::path *path;
-    std::fstream fp;
+    png_structp png_ptr;
+    png_infop   info_ptr;
+    png_bytep  *rowPtrs;
     
-    bool validate();
+
+    long width;
+    long height;
+
+    void save(boost::filesystem::path p);
+    void load();
+    
+    bool validate_stream();
+    bool init_png_structs();
+
+    std::fstream fp;
 	};
 }
 
