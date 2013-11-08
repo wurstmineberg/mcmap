2d4d365a4a7c5b911913499575b7cce81d6158c4
farthen <monkey@farthen.de>
2013-11-08 01:09:56 +0100

    Forgot to commit block_data.hpp

---
+++ src/block_data.hpp
@@ -0,0 +1,49 @@
+#ifndef __block_data_hpp
+#define __block_data_hpp
+
+namespace mcmap
+{
+  typedef struct block_info
+  {
+    char data_value;
+    int  block_id;
+  } block_info_t;
+
+  typedef struct
+	{
+		int id;
+		int data;
+	} item_info;
+
+	typedef enum
+	{
+	  BLOCK_FACE_TOP,
+	  BLOCK_FACE_NORTH,
+	  BLOCK_FACE_WEST,
+	  BLOCK_FACE_SOUTH,
+	  BLOCK_FACE_EAST,
+	  BLOCK_FACE_BOTTOM
+	} block_face_t;
+
+	typedef enum {
+		ITEM_TYPE_ITEM,
+		ITEM_TYPE_BLOCK
+	} item_type_t;
+
+	typedef enum {
+		ITEM_SUBTYPE_SLAB,
+		ITEM_SUBTYPE_SLAB_TOP,
+		ITEM_SUBTYPE_STAIRS
+	} item_subtype_t;
+
+	typedef struct {
+		texture *top;
+		texture *north;
+		texture *west;
+		texture *south;
+		texture *east;
+		texture *bottom;
+	} item_metadata_texture_list_t;
+}
+
+#endif
